c9121dd6cd1a32a8147fc8f94a0f8d6e0cc07912
yuliana-rodriguezm <ye.rodriguezm@uea.edu.ec>
2026-02-21 15:01:39 -0500

    Agregando ajustes según lo visto en la clase del Sábado

--- Tienda_maquillaje/servicios/inventario_service.py
+++ Tienda_maquillaje/servicios/inventario_service.py
@@ -2,23 +2,36 @@
 # Maneja CRUD y archivos
 # Permite agregar, listar, buscar, actualizar y eliminar productos del inventario
 # Además, guarda y carga el inventario desde un archivo de texto para persistencia de datos
-
+import os
 from modelos.producto_cosmetico import ProductoCosmetico
 
 class InventarioService:
 
     def __init__(self):
+        # Ruta del archivo de inventario txt
+        base_dir = os.path.dirname(__file__)
+        self.ruta_archivo = os.path.join(base_dir, "..", "registros", "inventario.txt")
+        
         self.productos = []
-        self.archivo = "inventario_cosmeticos.txt"
-        self.cargar_archivo()   # <-- CARGA AUTOMÁTICA
+        self.asegurar_archivo()   # ASEGURA QUE EL ARCHIVO EXISTA
+        self.cargar_desde_archivo()   # CARGA AUTOMÁTICA
 
     # ================= ARCHIVOS =================
 
-    # Guarda los productos del inventario en un archivo de texto usando "with"
-    # With asegura que el archivo se cierre automáticamente
-    def guardar_archivo(self):
+    def asegurar_archivo(self):
+        # Crea la carpeta y el archivo si no existen
+        carpeta = os.path.dirname(self.ruta_archivo)
+        
+        if not os.path.exists(carpeta):
+            os.makedirs(carpeta)
+        if not os.path.exists(self.ruta_archivo):
+            with open(self.ruta_archivo, "w", encoding="utf-8") as f:
+                pass  # Crea un archivo vacío
+
+    def guardar_en_archivo(self):
+        #Todos los productos se guardan en el archivo txt
         try:
-            with open(self.archivo, "w", encoding="utf-8") as f:
+            with open(self.ruta_archivo, "w", encoding="utf-8") as f:
                 for p in self.productos:
                     linea = f"{p.get_id()}|{p.get_nombre()}|{p.get_cantidad()}|{p.get_precio()}|{p.get_categoria()}\n"
                     f.write(linea)
@@ -30,9 +43,9 @@ class InventarioService:
 
     # Carga los productos desde ek archivo al iniciar el programa
     # Si el archivo no existe, se crea automáticamente
-    def cargar_archivo(self):
+    def cargar_desde_archivo(self):
         try:
-            with open(self.archivo, "r", encoding="utf-8") as f:
+            with open(self.ruta_archivo, "r", encoding="utf-8") as f:
                 for linea in f:
                     datos = linea.strip().split("|")
 
@@ -78,11 +91,11 @@ class InventarioService:
 
             nuevo = ProductoCosmetico(id_p, nombre, cantidad, precio, categoria)
             self.productos.append(nuevo)
-            self.guardar_archivo()
+            self.guardar_en_archivo()
             print("Producto agregado y guardado en archivo")
 
         except ValueError:
-            print("Datos inválidos")
+            print("ERROR: Datos inválidos")
 
     # LISTAR
     def listar_productos(self):
@@ -114,15 +127,19 @@ class InventarioService:
 
             nueva_cantidad = int(input("Nueva cantidad: "))
             nuevo_precio = float(input("Nuevo precio: "))
+            nuevo_nombre = input("Nuevo nombre: ")
+            nueva_categoria = input("Nueva categoría: ")
 
             producto.set_cantidad(nueva_cantidad)
             producto.set_precio(nuevo_precio)
-            self.guardar_archivo()
-
+            producto.set_nombre(nuevo_nombre)
+            producto.set_categoria(nueva_categoria)
+            
+            self.guardar_en_archivo()
             print("Producto actualizado y guardado")
 
         except ValueError:
-            print("Datos inválidos")
+            print("ERROR: Datos inválidos")
 
     # ELIMINAR
     def eliminar_producto(self):
@@ -135,8 +152,8 @@ class InventarioService:
                 return
 
             self.productos.remove(producto)
-            self.guardar_archivo()
-            print("Producto eliminado del inventario y archivo")
+            self.guardar_en_archivo()
+            print("Producto eliminado")
 
         except ValueError:
-            print("ID inválido")
+            print("ERROR: ID inválido")
